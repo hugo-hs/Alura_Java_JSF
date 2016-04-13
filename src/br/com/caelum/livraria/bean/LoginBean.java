dce780a81630ff51970015c33edf22ae92198c57
hugo souza <hugo souza>
2016-04-12 22:38:28 -0300

    Atenticao e Autorização do Usuario

---
+++ src/br/com/caelum/livraria/bean/LoginBean.java
@@ -0,0 +1,35 @@
+package br.com.caelum.livraria.bean;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+
+import br.com.caelum.livraria.dao.UsuarioDao;
+import br.com.caelum.livraria.modelo.Usuario;
+
+@ManagedBean
+@ViewScoped
+public class LoginBean {
+
+    private Usuario usuario = new Usuario();
+
+    public String efetuaLogin() {
+        System.out.println("Fazendo login do usuário "
+                + this.usuario.getEmail());
+
+        FacesContext context = FacesContext.getCurrentInstance();
+        boolean existe = new UsuarioDao().existe(this.usuario);
+
+        if (existe) {
+        	
+        	context.getExternalContext().getSessionMap().put("usuarioLogado", this.usuario);
+            return "livro?faces-redirect=true";
+        }
+        
+        return null;
+    }
+    
+    public Usuario getUsuario() {
+        return usuario;
+    }
+}
