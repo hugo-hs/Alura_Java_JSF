14d28d6333c353c11d2953b3e29b9f73638807de
hugo souza <hugo souza>
2016-05-02 22:49:11 -0300

    Melhorando a navegação com menus e aplicando temas

---
+++ src/br/com/caelum/livraria/bean/TemaBean.java
@@ -0,0 +1,35 @@
+package br.com.caelum.livraria.bean;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+public class TemaBean {
+
+    private String tema = "aristo"; //tema padrão
+    
+    private String[] temas;
+
+    public String getTema() {
+        return tema;
+    }
+
+    public void setTema(String tema) {
+        this.tema = tema;
+    }
+    
+    public String[] getTemas() {
+    	
+    	if(temas == null)
+    		temas = new String[] { "afterdark", "afternoon", "afterwork", "aristo",
+                "black-tie", "blitzer", "bluesky", "bootstrap", "casablanca",
+                "cupertino", "cruze", "dark-hive", "delta", "dot-luv",
+                "eggplant", "excite-bike", "flick", "glass-x", "home",
+                "hot-sneaks", "humanity", "le-frog", "midnight", "mint-choc",
+                "overcast", "pepper-grinder", "redmond", "rocket", "sam",
+                "smoothness", "south-street", "start", "sunny", "swanky-purse",
+                "trontastic", "ui-darkness", "ui-lightness", "vader" };
+    		return temas;
+        }
+}
